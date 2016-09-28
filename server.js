0f9c5fac4f27f7427e3f27f5699a71089d9c56b2
PhysicsMathsMan <mr.adithya.atg@gmail.com>
2016-09-28 18:07:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one', function(req, res){
+    res.send('Aritcel one will be severed here.')
+});
+
+
+app.get('/article-two', function(req, res){
+    res.send('Aritcel one will be severed here.')
+});
+
+
+app.get('/article-three', function(req, res){
+    res.send('Aritcel one will be severed here.')
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
